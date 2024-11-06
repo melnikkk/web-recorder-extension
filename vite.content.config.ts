a143d64b88ea8980b7393f0a4b776f1235682703
Aleksander Melnik <alexander.melnik@pandadoc.com>
2024-11-06 23:04:07 +0100

    Connected content script

---
+++ vite.content.config.ts
@@ -0,0 +1,16 @@
+import {defineConfig} from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    emptyOutDir: false,
+    rollupOptions: {
+      input: {
+        content: "src/content-scripts/content.ts"
+      },
+      output: {
+        entryFileNames: "content-scripts/[name].js"
+      }
+    }
+  }
+})
