e20ced95c26bf50cef126574179b2796d3ac0ccd
Alexander Melnik <47505999+melnikkk@users.noreply.github.com>
2025-05-04 17:05:40 +0200

    [fix] fixed uninitialised port error

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,5 +1,17 @@
 import { eventListener } from './eventListener';
+import { BackgroundMessage } from '../types';
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.runtime.onMessage.addListener(eventListener);
+chrome.runtime.onMessage.addListener((message: BackgroundMessage, _sender, sendResponse) => {
+  (async () => {
+    try {
+      await eventListener(message);
+      
+      sendResponse({ success: true });
+    } catch (error) {
+      console.error('Error processing message:', error);
+      sendResponse({ success: false, error });
+    }
+  })();
+  
+  return true;
 });
